8a97f07d9b3d835f0603c27dd2c1dd078b01ceed
Dan Goodman <xxdanthegoodmanxx@gmail.com>
2022-03-31 13:45:28 -0400

    add matchmaking

--- gubernator.go
+++ gubernator.go
@@ -51,3 +51,15 @@ func GenTransactionConfig(uniqueKey string) *gubernator.RateLimitReq {
 		Behavior:  0,
 	}
 }
+
+func GenMatchmakingonfig(uniqueKey string) *gubernator.RateLimitReq {
+	return &gubernator.RateLimitReq{
+		Name:      "matchmaking",
+		UniqueKey: uniqueKey,
+		Hits:      1,
+		Limit:     15, // 15/min
+		Duration:  60000,
+		Algorithm: 0,
+		Behavior:  0,
+	}
+}
